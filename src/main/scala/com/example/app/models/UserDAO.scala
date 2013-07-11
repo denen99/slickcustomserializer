2e6be133f431c406127d2e115c25a198ad36b647
Adam Denenberg <adenenberg@adenenbergs-MacBook-Air-2.local>
2013-07-11 10:06:00 -0400

    cleaned up DAO

--- src/main/scala/com/example/app/models/UserDAO.scala
+++ src/main/scala/com/example/app/models/UserDAO.scala
@@ -3,32 +3,14 @@ package com.example.app.models
 import com.example.app.SlickInit
 import scala.slick.driver.MySQLDriver.simple._
 import Database.threadLocalSession
-import scala.slick.jdbc.{GetResult, StaticQuery => Q}
-import Q.interpolation
+//import scala.slick.jdbc.{GetResult, StaticQuery => Q}
+//import Q.interpolation
 
 
-class BaseDAO[T](table: String) {
-
-  //implicit val getUserResult = GetResult(r => User(r.<<, r.<<))
 
 
- // def find2(id: Int): Option[T] = {
-   // SlickInit.dbMaster withSession {
-      //      val q = for {
-      //        x <- Users if x.id === id
-      //      } yield x
-      //      q.firstOption.map { case(id,email) =>  User(id,email) }
-      //sql"select * from $table where id = $id".as[T].firstOption
-    //}
- // }
 
-}
-
-
-object UserDAO extends BaseDAO[User]("users") {
-
-  implicit val getUserResult = GetResult(r => User(r.<<, r.<<))
-
+object UserDAO  {
 
   def find(id: Int): Option[User] = {
     SlickInit.dbMaster withSession {
